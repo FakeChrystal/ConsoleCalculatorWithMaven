ad797e5d0aed29a7766e8446e97acd844993c846
FakeChrystal <richard.j.hauri@gmail.com>
2018-01-10 09:19:25 +0100

    Implement addition and subtraction for fun

---
+++ src/main/java/ch/bsfh/richardhauri/Calculator.java
@@ -0,0 +1,17 @@
+package ch.bsfh.richardhauri;
+
+/**
+ * Calculator
+ * Addition and Subtraction
+ * @author richa
+ * @version 0.0.1
+ */
+
+public class Calculator {
+	public int addition(int a, int b) {
+		return a + b;
+	}
+	public int subtraction(int a, int b) {
+		return a - b;
+	}
+}
